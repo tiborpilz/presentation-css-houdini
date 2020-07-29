f03d4bf8b21952c50a5c4bb906590df59855b556
Tibor Pilz <tibor.pilz@sevenval.com>
2020-07-29 18:26:51 +0200

    add simple animation

--- examples/paint/script.js
+++ examples/paint/script.js
@@ -1,1 +1,8 @@
 CSS.paintWorklet.addModule('avenga.js');
+
+function animate() {
+  document.querySelector('.avenga-footer').style.setProperty('--timestep', new Date().getTime() % 1000);
+  requestAnimationFrame(animate)
+}
+
+animate()
